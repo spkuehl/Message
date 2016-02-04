0b0af47ddc2c039bbd5ef14890ffe7f7f9744b60
spkuehl <skuehlhorn@gmail.com>
2016-02-03 18:28:41 -0600

    Testing mention and topic getter

--- parser.py
+++ parser.py
@@ -1,8 +1,21 @@
+from re import findall
+
+
 class Tweet:
     """A simple Tweet parser class"""
+
     def __init__(self, tweet):
         self.text = tweet
 
-tweet = Tweet('Hello World')
+    def get_mentions(self):
+        findall(r"@\w+", self.text)
+
+    def get_topics(self):
+        findall(r"Hello", self.text)
+
+
+tweet = Tweet('Hello #world @twitter')
 print(tweet.__doc__)
 print(tweet.text)
+print(tweet.get_mentions())
+print(tweet.get_topics())
